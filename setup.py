adb3905c4248613f1c9b88c6b507387c613bddd9
XThexLonexWolfX <71769542+XThexLonexWolfX@users.noreply.github.com>
2024-12-11 16:33:14 +0100

    Fixed comma in version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="IngeniumUAHub-GoogleAPI",
-    version="1,0",
+    version="1.0",
     description="A package to integrate Google API functionalities for Calendar, Drive, Mailing, and Workspace",
     author="Yorben Joosen",
     author_email="webmaster@ingeniumua.be",
