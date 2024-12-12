cbd7646b6e435aa182e5262c4cb7eba27afe8d03
XThexLonexWolfX <71769542+XThexLonexWolfX@users.noreply.github.com>
2024-12-12 17:27:36 +0100

    Testing

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 setup(
     name="IngeniumUAHub-GoogleAPI",
-    version="8.0",
+    version="9.0",
     description="A package to integrate Google API functionalities for Calendar, Drive, Gmail, and Workspace",
     author="Yorben Joosen",
     author_email="webmaster@ingeniumua.be",
     url="https://github.com/IngeniumUA/IngeniumUAHub-GoogleAPI",
-    packages=find_packages(),
+    packages=["IngeniumUAHub-GoogleAPI"],
     install_requires=[
     ],  # external packages as dependencies
 )
