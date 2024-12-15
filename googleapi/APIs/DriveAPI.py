6dd66a43fc5dbf44a09d1b2a7bc8a463bc293477
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2024-12-15 18:47:03 +0100

    Return the updated groups/users/... now so a user can save the IDs

---
+++ googleapi/APIs/DriveAPI.py
@@ -0,0 +1,84 @@
+import json
+from os import path as os_path
+from typing import List, cast
+
+import aiogoogle.excs
+from aiogoogle import Aiogoogle
+from aiogoogle.auth.creds import ServiceAccountCreds
+
+from googleapi.TypedDicts.Drive import DriveModel, DrivesModel
+
+
+class Drive:
+    """
+    Implements the Google Drive API to manipulate Google Drive.
+    """
+
+    def __init__(self, service_file_path: str, subject: str) -> None:
+        """
+        @param service_file_path: Path to the service account credentials file
+        @param subject: Subject who owns the drive
+        """
+        self.scopes = ["https://www.googleapis.com/auth/drive"]
+        self.timeZone = "Europe/Brussels"
+        self.subject = subject
+
+        if not os_path.exists(service_file_path):
+            raise Exception("Service account json path does not exist")
+
+        self.serviceFilePath = service_file_path
+        self.service_account_credentials = self._build_service_account_credentials()
+
+    def _build_service_account_credentials(self) -> ServiceAccountCreds:
+        """
+        @return: Returns ServiceAccountCreds from aiogoogle
+        """
+        service_account_key = json.load(open(self.serviceFilePath))
+        credentials = ServiceAccountCreds(
+            scopes=self.scopes, **service_account_key, subject=self.subject
+        )
+        return credentials
+
+    async def get_drives(self) -> List[DriveModel]:
+        """
+        Gets the drives of the user
+        @return: Drives of the user
+        """
+        try:
+            async with (Aiogoogle(
+                    service_account_creds=self.service_account_credentials
+            ) as google):
+                drive = await google.discover("drive", "v3")
+                return cast(DrivesModel, await google.as_service_account(drive.drives.list())).get("drives", [])
+        except aiogoogle.excs.HTTPError as error:
+            raise Exception("Aiogoogle error") from error
+
+    async def get_drive(self, drive_id: str) -> DriveModel:
+        """
+        Gets the drive of the user
+        @param drive_id: ID of the drive
+        @return: Drive of the user
+        """
+        try:
+            async with (Aiogoogle(
+                    service_account_creds=self.service_account_credentials
+            ) as google):
+                drive = await google.discover("drive", "v3")
+                return cast(DriveModel, await google.as_service_account(drive.drives.get(driveId=drive_id)))
+        except aiogoogle.excs.HTTPError as error:
+            raise Exception("Aiogoogle error") from error
+
+    async def delete_drive(self, drive_id) -> None:
+        """
+        Deletes the drive of the user
+        @param drive_id: ID of the drive
+        @return: Nothing
+        """
+        try:
+            async with (Aiogoogle(
+                    service_account_creds=self.service_account_credentials
+            ) as google):
+                drive = await google.discover("drive", "v3")
+                await google.as_service_account(drive.drives.delete(driveId=drive_id))
+        except aiogoogle.excs.HTTPError as error:
+            raise Exception("Aiogoogle error") from error
