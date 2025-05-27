0610eff704b539b652ae8431abf0849754818a92
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2025-05-27 23:04:28 +0200

    Made method names more clear

--- googleapi/APIs/GeocodingAPI.py
+++ googleapi/APIs/GeocodingAPI.py
@@ -6,7 +6,7 @@ class Geocoding:
         self.base_url = "https://maps.googleapis.com/maps/api/geocode/json"
         self.api_key = api_key
 
-    async def get_coordinates(self, address: str) -> (float, float):
+    async def get_coordinates_from_address(self, address: str) -> (float, float):
         """
         :param address: Address to geocode
         :return: Tuple of latitude and longitude (latitude, longitude)
@@ -27,7 +27,7 @@ class Geocoding:
                 location = data.get("results")[0].get("geometry").get("location")
                 return location.get("lat"), location.get("lng")
 
-    async def get_address(self, latitude: float, longitude: float) -> str:
+    async def get_address_from_coordinates(self, latitude: float, longitude: float) -> str:
         """
         :param latitude: Latitude of the location
         :param longitude: Longitude of the location
