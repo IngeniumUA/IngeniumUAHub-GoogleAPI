641ab201bdbc5f1b3f86c5e09be8fe58088c65b7
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2025-05-27 22:42:37 +0200

    Added documentation and get location method

--- googleapi/APIs/GeocodingAPI.py
+++ googleapi/APIs/GeocodingAPI.py
@@ -7,6 +7,10 @@ class Geocoding:
         self.api_key = api_key
 
     async def get_coordinates(self, address: str) -> (float, float):
+        """
+        :param address: Address to geocode
+        :return: Tuple of latitude and longitude (latitude, longitude)
+        """
         parameters = {"address": address, "key": self.api_key}
         async with aiohttp.ClientSession() as session:
             async with session.get(self.base_url, params=parameters) as response:
@@ -22,3 +26,28 @@ class Geocoding:
 
                 location = data.get("results")[0].get("geometry").get("location")
                 return location.get("lat"), location.get("lng")
+
+    async def get_address(self, latitude: float, longitude: float) -> str:
+        """
+        :param latitude: Latitude of the location
+        :param longitude: Longitude of the location
+        :return: The address corresponding to the given coordinates
+        """
+        parameters = {
+            "latlng": f"{latitude},{longitude}",
+            "key": self.api_key,
+        }
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.base_url, params=parameters) as response:
+                if response.status != 200:
+                    raise Exception(
+                        f"Request failed with status code {response.status}"
+                    )
+
+                data = await response.json()
+
+                if data["status"] != "OK":
+                    raise Exception(f"Geocoding failed: {data.get('status')}")
+
+                location = data.get("results")[0].get("formatted_address")
+                return location
