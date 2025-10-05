68e6d47175f2f79d4d22779487e9e798ee01841f
XThexLonexWolfX <yorben_joosen01@hotmail.com>
2025-10-05 18:02:01 +0200

    Extra debugging

--- googleapi/Helpers/HelperFunctions.py
+++ googleapi/Helpers/HelperFunctions.py
@@ -63,7 +63,8 @@ async def execute_aiogoogle(
                 )
             else:
                 api = await google.discover(api_name, api_version, disco_doc_ver=2)
-
+            response = await google.as_service_account(method_callable(api, **method_args))
+            print(response)
             return await google.as_service_account(method_callable(api, **method_args))
     except aiogoogle.excs.HTTPError as error:
         raise HTTPException(
